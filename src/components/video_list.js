5cd7ea5136fe78b8bd1dd6126cf7ae01d21dad4a
Gabriela Gil <gabrielagil247@gmail.com>
2018-05-06 21:53:01 -0500

    adding search functionality

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = (props) => {
-    const videoItems = props.videos.map((video) => {
-        return <VideoListItem key={video.etag} video={video} />
+const VideoList = ({videos, onVideoSelect}) => {
+    const videoItems = videos.map((video) => {
+        return (
+            <VideoListItem 
+                onVideoSelect={onVideoSelect}
+                key={video.etag} 
+                video={video} />
+        );
     });
 
     return (
         <ul className="col-md-4 list-group">
             {videoItems}
-        </ul>
+        </ul> 
      );
  };
 
